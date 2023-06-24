86670ce2264fa49079f37f322be7340bae06d98a
Farith Palestina <palestinagodinez@gmail.com>
2023-06-23 22:06:59 -0600

    Github Pages for vte

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    base: 'https://farith-palestina.github.io/Profile-card-component-vite-with-vanilla-react'
 })
